d8b6522a804a75cc4b8f700d36882b14a3a0e756
Michelle Zelaya <michellecastro2122@gmail.com>
2026-02-04 14:22:15 -0600

    Se agrego la funcionalidad de la calculadora basica

--- miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
+++ miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
@@ -7,14 +7,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import android.widget.Button;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
-
+    TextView tempVal;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
+        btn = findViewById(R.id.btnCalcular);
+        btn.setOnClickListener(v->calcular());
+    }
+    private void calcular(){
+        tempVal = findViewById(R.id.txtNum1);
+        Double num1 = Double.parseDouble(tempVal.getText().toString());
+
+        tempVal = findViewById(R.id.txtNum2);
+        Double num2 = Double.parseDouble(tempVal.getText().toString());
+
+        double respuesta = num1 + num2;
+
+        tempVal = findViewById(R.id.lblRespuesta);
+        tempVal.setText("Respuesta: "+ respuesta);
     }
 }
