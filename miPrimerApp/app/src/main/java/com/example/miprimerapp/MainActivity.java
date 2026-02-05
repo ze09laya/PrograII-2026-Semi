18244604555c3fbfc7942d78cecfe379e2c708b9
Michelle Zelaya <michellecastro2122@gmail.com>
2026-02-05 17:36:17 -0600

    Le agregamos la opcion es de suma, resta, multiplicacion, division.

--- miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
+++ miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
@@ -8,10 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     TextView tempVal;
     Button btn;
+    RadioButton opt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +30,42 @@ public class MainActivity extends AppCompatActivity {
         tempVal = findViewById(R.id.txtNum2);
         Double num2 = Double.parseDouble(tempVal.getText().toString());
 
-        double respuesta = num1 + num2;
+        double respuesta = 0;
+        opt = findViewById(R.id.optSuma);
+        if(opt.isChecked()) {
+        respuesta = num1 + num2;
+        }
+
+        opt = findViewById(R.id.optResta);
+        if(opt.isChecked()) {
+            respuesta = num1 - num2;
+        }
+
+        opt = findViewById(R.id.optMultiplicar);
+        if(opt.isChecked()) {
+            respuesta = num1 * num2;
+        }
+
+        opt = findViewById(R.id.optDividir);
+        if(opt.isChecked()) {
+            respuesta = num1 / num2;
+        }
+
+        opt = findViewById(R.id.optFactorial);
+        if(opt.isChecked()) {
+
+            long fact = 1;
+            for(int i = 1; i <= num1; i++){
+                fact = fact * i;
+            }
+            respuesta = fact;
+        }
+
+        opt = findViewById(R.id.optPorcentaje);
+        if (opt.isChecked()) {
+            respuesta = (num1 * num2) / 100;
+        }
+
 
         tempVal = findViewById(R.id.lblRespuesta);
         tempVal.setText("Respuesta: "+ respuesta);
