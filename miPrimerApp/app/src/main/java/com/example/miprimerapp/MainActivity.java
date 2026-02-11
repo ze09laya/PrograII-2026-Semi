59fd313ffe6d1d6a3cd5f51f389a1d32342438f5
Michelle Zelaya <michellecastro2122@gmail.com>
2026-02-11 17:04:20 -0600

    Creacion del conversor de monedas usando tabhost

--- miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
+++ miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
@@ -3,12 +3,21 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.Button;
+import androidx.activity.EdgeToEdge;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    TabHost tbh;
+    TextView tempVal;
+    Spinner spn;
+    Button btn;
+    Double valores[] = new Double[] {1.0, 0.85, 7.67, 26.42, 36.80, 495.77};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        tbh = findViewById(R.id.tbhConversores);
+        tbh.setup();
+
+        tbh.addTab(tbh.newTabSpec("Monedas").setContent(R.id.tabmonedas).setIndicator("MONEDAS", null));
+        tbh.addTab(tbh.newTabSpec("Longitud").setContent(R.id.tablongitud).setIndicator("LONGITUD", null));
+        tbh.addTab(tbh.newTabSpec("Volumen").setContent(R.id.tabvolumen).setIndicator("VOLUMEN", null));
+        tbh.addTab(tbh.newTabSpec("Masa").setContent(R.id.tabmasa).setIndicator("MASA", null));
+
+        btn = findViewById(R.id.btnMonedasConvertir);
+        btn.setOnClickListener(v->convertirMonedas());
+    }
+    private void convertirMonedas(){
+        spn = findViewById(R.id.spnMonedasDe);
+        int de = spn.getSelectedItemPosition();
+
+        spn = findViewById(R.id.spnMonedasA);
+        int a = spn.getSelectedItemPosition();
+
+        tempVal = findViewById(R.id.txtMonedasCantidad);
+        double cantidad = Double.parseDouble(tempVal.getText().toString());
+        double respuesta = conversor(de, a, cantidad);
+
+        tempVal = findViewById(R.id.lblMonedasRespuesta);
+        tempVal.setText("Respuesta: "+ respuesta);
+    }
+    double conversor(int de, int a, double cantidad){
+        return valores[a]/valores[de] * cantidad;
 
 
     }
