54e63ffdab9e015490105f739f1c54e6912d8776
Michelle Zelaya <michellecastro2122@gmail.com>
2026-02-11 14:37:43 -0600

    Usando el RadioGroup y el Id para saber cual operacion es la que se va a realizar

--- miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
+++ miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
@@ -1,18 +1,24 @@
+
 package com.example.miprimerapp;
-
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
     TextView tempVal;
     Button btn;
+    RadioGroup radioGroup;
+
+
     RadioButton opt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,28 +37,24 @@ public class MainActivity extends AppCompatActivity {
         Double num2 = Double.parseDouble(tempVal.getText().toString());
 
         double respuesta = 0;
-        opt = findViewById(R.id.optSuma);
-        if(opt.isChecked()) {
-        respuesta = num1 + num2;
+
+        radioGroup = findViewById(R.id.optOpciones);
+        if(radioGroup.getCheckedRadioButtonId()==R.id.optSuma) {
+            respuesta = num1 + num2;
         }
 
-        opt = findViewById(R.id.optResta);
-        if(opt.isChecked()) {
+        if(radioGroup.getCheckedRadioButtonId()==R.id.optResta) {
             respuesta = num1 - num2;
         }
-
-        opt = findViewById(R.id.optMultiplicar);
-        if(opt.isChecked()) {
+        if(radioGroup.getCheckedRadioButtonId()==R.id.optMultiplicar)  {
             respuesta = num1 * num2;
         }
 
-        opt = findViewById(R.id.optDividir);
-        if(opt.isChecked()) {
+        if(radioGroup.getCheckedRadioButtonId()==R.id.optDividir) {
             respuesta = num1 / num2;
         }
 
-        opt = findViewById(R.id.optFactorial);
-        if(opt.isChecked()) {
+        if(radioGroup.getCheckedRadioButtonId()==R.id.optFactorial) {
 
             long fact = 1;
             for(int i = 1; i <= num1; i++){
@@ -61,13 +63,33 @@ public class MainActivity extends AppCompatActivity {
             respuesta = fact;
         }
 
-        opt = findViewById(R.id.optPorcentaje);
-        if (opt.isChecked()) {
+        if(radioGroup.getCheckedRadioButtonId()==R.id.optPorcentaje) {
             respuesta = (num1 * num2) / 100;
         }
 
+        if(radioGroup.getCheckedRadioButtonId()==R.id.optExponenciacion) {
+
+            double pot = 1;
+
+            for (int i = 1; i <= num2; i++) {
+                pot = pot * num1;
+            }
+
+            respuesta = pot;
+        }
+
+        if(radioGroup.getCheckedRadioButtonId()==R.id.optRaiz){
+
+            if (num1 >= 0) {
+                respuesta = Math.sqrt(num1);
+            } else {
+                tempVal.setText("No existe raíz de números negativos");
+                return;
+            }
+        }
 
         tempVal = findViewById(R.id.lblRespuesta);
         tempVal.setText("Respuesta: "+ respuesta);
     }
 }
+
