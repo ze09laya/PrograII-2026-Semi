f3e1bed33b0177a448e725e2d1d2a2d67ab65953
Michelle Zelaya <michellecastro2122@gmail.com>
2026-02-11 15:23:08 -0600

    Usando el spinner para las operaciones

--- miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
+++ miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
@@ -1,95 +1,86 @@
-
 package com.example.miprimerapp;
 import android.os.Bundle;
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
     TextView tempVal;
     Button btn;
-    RadioGroup radioGroup;
+    Spinner spn;
 
-
-    RadioButton opt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(v->calcular());
+        btn.setOnClickListener(v -> calcular());
     }
-    private void calcular(){
+
+    private void calcular() {
+
         tempVal = findViewById(R.id.txtNum1);
-        Double num1 = Double.parseDouble(tempVal.getText().toString());
+        double num1 = Double.parseDouble(tempVal.getText().toString());
 
         tempVal = findViewById(R.id.txtNum2);
-        Double num2 = Double.parseDouble(tempVal.getText().toString());
+        double num2 = Double.parseDouble(tempVal.getText().toString());
 
         double respuesta = 0;
 
-        radioGroup = findViewById(R.id.optOpciones);
-        if(radioGroup.getCheckedRadioButtonId()==R.id.optSuma) {
-            respuesta = num1 + num2;
-        }
+        spn = findViewById(R.id.cboOpciones);
 
-        if(radioGroup.getCheckedRadioButtonId()==R.id.optResta) {
-            respuesta = num1 - num2;
-        }
-        if(radioGroup.getCheckedRadioButtonId()==R.id.optMultiplicar)  {
-            respuesta = num1 * num2;
-        }
+        switch (spn.getSelectedItemPosition()) {
 
-        if(radioGroup.getCheckedRadioButtonId()==R.id.optDividir) {
-            respuesta = num1 / num2;
-        }
+            case 0: // suma
+                respuesta = num1 + num2;
+                break;
 
-        if(radioGroup.getCheckedRadioButtonId()==R.id.optFactorial) {
+            case 1: // Resta
+                respuesta = num1 - num2;
+                break;
 
-            long fact = 1;
-            for(int i = 1; i <= num1; i++){
-                fact = fact * i;
-            }
-            respuesta = fact;
-        }
+            case 2: // Multiplicacion
+                respuesta = num1 * num2;
+                break;
 
-        if(radioGroup.getCheckedRadioButtonId()==R.id.optPorcentaje) {
-            respuesta = (num1 * num2) / 100;
-        }
+            case 3: // division
+                respuesta = num1 / num2;
+                break;
 
-        if(radioGroup.getCheckedRadioButtonId()==R.id.optExponenciacion) {
+            case 4: // Factorial
+                int n = (int) num1;
+                long fact = 1;
+                for (int i = 1; i <= n; i++) {
+                    fact *= i;
+                }
+                respuesta = fact;
+                break;
 
-            double pot = 1;
+            case 5: // Porcentaje
+                respuesta = (num1 * num2) / 100;
+                break;
 
-            for (int i = 1; i <= num2; i++) {
-                pot = pot * num1;
-            }
+            case 6: // Exponenciación
+                respuesta = Math.pow(num1, num2);
+                break;
 
-            respuesta = pot;
-        }
-
-        if(radioGroup.getCheckedRadioButtonId()==R.id.optRaiz){
-
-            if (num1 >= 0) {
-                respuesta = Math.sqrt(num1);
-            } else {
-                tempVal.setText("No existe raíz de números negativos");
-                return;
-            }
+            case 7: // Raíz
+                if (num1 >= 0) {
+                    respuesta = Math.sqrt(num1);
+                } else {
+                    tempVal = findViewById(R.id.lblRespuesta);
+                    tempVal.setText("No existe raíz de números negativos");
+                    return;
+                }
+                break;
         }
 
         tempVal = findViewById(R.id.lblRespuesta);
-        tempVal.setText("Respuesta: "+ respuesta);
+        tempVal.setText("Respuesta: " + respuesta);
     }
 }
-
