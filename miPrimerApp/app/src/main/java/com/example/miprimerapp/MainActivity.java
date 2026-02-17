a49f8fdfaf147d4e38fdc591b49c9981f67e5f09
Michelle Zelaya <michellecastro2122@gmail.com>
2026-02-17 16:55:40 -0600

    Nueva rama de conversor usando buenas practicas de progrmacion, usando POO, y matrices

--- miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
+++ miPrimerApp/app/src/main/java/com/example/miprimerapp/MainActivity.java
@@ -2,89 +2,97 @@ package com.example.miprimerapp;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.TabHost;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import androidx.activity.EdgeToEdge;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
+
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    TabHost tbh;
-    TextView lbl;
+    TextView tempVal;
     Spinner spn;
     Button btn;
 
-    Double longitudes[] = {1.0, 1000.0, 100.0, 39.3701, 3.28084, 1.1963, 1.09361};
+    Double valores[][] = {
+            {1.0, 0.85, 7.67, 26.42, 36.80, 495.77}, //monedas
+            {1.0, 1000.0, 100.0, 39.3701, 3.280841666667, 1.1963081929167, 1.09361}, //longitud
+            {}, //volumen
+    };
+    String[][] etiquetas = {
+            {"Dolar", "Euro", "Quetzal", "Lempira", "Cordoba", "Colon CR"}, //monedas
+            {"Mts", "Ml", "Cm", "Pulgada", "Pies", "Vara", "Yarda"}, //Longitud
+            {""},  //volumen
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tbh = findViewById(R.id.tbhConversores);
-        tbh.setup();
+        btn = findViewById(R.id.btnConvertir);
+        btn.setOnClickListener(v -> convertir());
 
-        tbh.addTab(tbh.newTabSpec("Monedas").setContent(R.id.tabmonedas).setIndicator("", getDrawable(R.drawable.monedas)));
-        tbh.addTab(tbh.newTabSpec("Longitud").setContent(R.id.tablongitud).setIndicator("", getResources().getDrawable(R.drawable.longitud)));
-        tbh.addTab(tbh.newTabSpec("Volumen").setContent(R.id.tabvolumen).setIndicator("", getDrawable(R.drawable.volumen)));
-        tbh.addTab(tbh.newTabSpec("Masa").setContent(R.id.tabmasa).setIndicator("", getDrawable(R.drawable.masa)));
+        cambiarEtiqueta(0);//valores predeterminaods
+        spn = findViewById(R.id.spnTipo);
+        spn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                cambiarEtiqueta(i);
+            }
 
-        btn = findViewById(R.id.btnLongitudConvertir);
-        btn.setOnClickListener(v -> convertirLongitud());
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
-        btn = findViewById(R.id.btnMonedasConvertir);
-        btn.setOnClickListener(v->convertirMonedas());
 
-        btn = findViewById(R.id.btnLongitudConvertir);
-        btn.setOnClickListener(v->convertirLongitud());
+            }
+        });
     }
 
-    private void convertirLongitud(){
-        spn = findViewById(R.id.spnLongitudDe);
+    private void cambiarEtiqueta(int posicion) {
+        ArrayAdapter<String> aaEtiquetas = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                etiquetas[posicion]
+        );
+        aaEtiquetas.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spn = findViewById(R.id.spnDe);
+        spn.setAdapter(aaEtiquetas);
+
+
+        spn = findViewById(R.id.spnA);
+        spn.setAdapter(aaEtiquetas);
+    }
+
+    private void convertir() {
+        spn = findViewById(R.id.spnTipo);
+        int tipo = spn.getSelectedItemPosition();
+
+        spn = findViewById(R.id.spnDe);
         int de = spn.getSelectedItemPosition();
 
-        spn = findViewById(R.id.spnLongitudA);
+        spn = findViewById(R.id.spnA);
         int a = spn.getSelectedItemPosition();
 
-        tempVal = findViewById(R.id.txtLongitudCantidad);
+        tempVal = findViewById(R.id.txtCantidad);
         double cantidad = Double.parseDouble(tempVal.getText().toString());
-        double respuesta = conversorLongitud(de, a, cantidad);
 
-        tempVal = findViewById(R.id.lblLongitudRespuesta);
-        tempVal.setText("Respuesta: "+ respuesta);
+        double respuesta = conversor(tipo, de, a, cantidad);
+
+        tempVal = findViewById(R.id.lblRespuesta);
+        tempVal.setText("Respuesta: " + respuesta);
     }
 
-    private void convertirMonedas(){
-        spn = findViewById(R.id.spnMonedasDe);
-        int de = spn.getSelectedItemPosition();
-
-        spn = findViewById(R.id.spnMonedasA);
-        int a = spn.getSelectedItemPosition();
-
-        tempVal = findViewById(R.id.txtMonedasCantidad);
-        double cantidad = Double.parseDouble(tempVal.getText().toString());
-        double respuesta = conversor(de, a, cantidad);
-
-        tempVal = findViewById(R.id.lblMonedasRespuesta);
-        tempVal.setText("Respuesta: "+ respuesta);
+    private double conversor(int tipo, int de, int a, double cantidad) {
+        return valores[tipo][a] / valores[tipo][de] * cantidad;
     }
 
-    double conversor(int de, int a, double cantidad){
-        return valores[a]/valores[de] * cantidad;
-    }
-
-    double conversorLongitud(int de, int a, double cantidad){
-        return longitudes[a]/longitudes[de] * cantidad;
-    }
 }
+
+
