5a645ccf117089bf12d8a5df99e64ec03162c840
Michelle Zelaya <michellecastro2122@gmail.com>
2026-04-28 19:25:15 -0600

    PARCIAL,CouchDB, SQLITE tienda productos. FINAL CON GANANCIA INCLUIDA CORRECION

--- miPrimerApp/app/src/main/java/com/example/miprimerapp/utilidades.java
+++ miPrimerApp/app/src/main/java/com/example/miprimerapp/utilidades.java
@@ -5,7 +5,7 @@ public class utilidades {
     static String url_consulta = "http://192.168.1.9:5984/michelle/_design/saul/_view/saul";
     static String url_mto = "http://192.168.1.9:5984/michelle"; //CRUD, Insertar, Actualizar, Borrar, y Buscar
     static String user = "michelle";
-    static String passwd = "12345";
+    static String passwd = "";
     static String credencialesCodificadas = Base64.getEncoder().encodeToString((user +":"+ passwd).getBytes());
     public String generarUnicoId(){
         return java.util.UUID.randomUUID().toString();
